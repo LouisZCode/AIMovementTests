97f7a7f043a874d6ed968addf52cb07b7b726323
LouisZCode <lgzg90@hotmail.com>
2025-10-19 20:36:39 +0200

    feature: add of simple running mechanic

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,14 +7,20 @@ extends CharacterBody2D
 func _physics_process(_delta):
 	# Get input
 	var direction = Input.get_axis("ui_left", "ui_right")
+	var is_running = Input.is_action_pressed("ui_shift")
+	
+	var current_speed = speed * 2 if is_running else speed
 	
 	# Move
-	velocity.x = direction * speed
+	velocity.x = direction * current_speed
 	move_and_slide()
 	
 	# Animations
 	if direction != 0:
-		anim.play("walking")
-		anim.flip_h = direction < 0  # Flip when moving left
+		if is_running:
+			anim.play("running")
+		else:
+			anim.play("walking")
+		anim.flip_h = direction < 0
 	else:
 		anim.play("idle")
