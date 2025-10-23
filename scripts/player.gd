e90ab29a825a1ef240fbab052e0744ecaa424d3b
LouisZCode <lgzg90@hotmail.com>
2025-10-23 17:10:43 +0200

    feature: Character facing direction follows Mouse + Add of Camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 
-@export var speed = 200.0
+@export var speed = 170.0
 
 @onready var anim = $player_animation
 
 func _physics_process(_delta):
+	# Get mouse position and determine facing
+	var mouse_pos = get_global_mouse_position()
+	var facing_right = mouse_pos.x > global_position.x
+	anim.flip_h = not facing_right  # Flip sprite based on mouse
+	
 	# Get input
 	var direction = Input.get_axis("ui_left", "ui_right")
 	var is_running = Input.is_action_pressed("ui_shift")
 	
-	var current_speed = speed * 2 if is_running else speed
+	# Set speed
+	var current_speed = speed * 2.5 if is_running else speed
 	
 	# Move
 	velocity.x = direction * current_speed
@@ -21,6 +27,5 @@ func _physics_process(_delta):
 			anim.play("running")
 		else:
 			anim.play("walking")
-		anim.flip_h = direction < 0
 	else:
 		anim.play("idle")
